3bac68eaaed671876ae5258c4daed758e37d8bf7
jbeans0312 <jbeans@udel.edu>
2025-08-27 21:17:19 -0400

    prepared site for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://jbeans0312.github.io',
+    base: '/schistostega'
+});
