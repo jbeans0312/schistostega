1ab93fa57b580a8dca6fc1394dcb6963f0f39096
jbeans0312 <jbeans@udel.edu>
2025-08-27 21:57:45 -0400

    updated for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jbeans0312.github.io',
-    base: '/schistostega'
+    site: 'www.schistostega.com',
 });
